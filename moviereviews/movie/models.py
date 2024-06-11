711e5a114775745f86b283e2a28eec733ea9efe9
Bula23 <markowski.ad@gmail.com>
2024-06-11 07:33:51 -0700

    nowa wersja v4

--- moviereviews/movie/models.py
+++ moviereviews/movie/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Movie(models.Model):
@@ -8,3 +9,12 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    text = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    watchAgain = models.BooleanField()
+    def __str__(self):
+        return self.text
